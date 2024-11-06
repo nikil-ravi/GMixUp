309bb82d3c1c28da8521e3dadeb48550e2538967
Nikil Ravi <nravi@stanford.edu>
2024-11-05 18:19:54 -0800

    graphon approximation- step function

--- gmixup.py
+++ gmixup.py
@@ -15,11 +15,52 @@ class GMixup(torch_geometric.datasets.graph_generator.GraphGenerator):
         """
         pass
 
-    def estimate_graphon():
+    def align_nodes(self, graph, criterion="degree"):
         """
-        Takes a class of graphs, returns a graphon representation for that class
+        Aligns nodes of a graph based on a criterion (e.g. degree, centrality, etc.)
         """
-        pass
+        if criterion == "degree":
+            node_degrees = graph.degree()
+            sorted_indices = np.argsort(node_degrees)
+            aligned_matrix = graph.adjacency_matrix[sorted_indices, :][:, sorted_indices]
+            return aligned_matrix
+        else:
+            raise NotImplementedError(f"Criterion {criterion} not implemented")
+
+
+    def estimate_graphon(self, graphs, K):
+        """
+        Takes a set of graphs, returns an approximation of the graphon for that set of graphs.
+        """
+        
+        # align adjacency matrices
+        aligned_adjacency_matrices = []
+        for graph in graphs:
+            aligned_matrix = self.align_nodes(graph)
+            aligned_adjacency_matrices.append(aligned_matrix)
+
+        # initialize step function matrix
+        step_function_matrix = np.zeros((K, K))
+
+        # partition nodes into K intervals; each interval will have N/K nodes
+        N = aligned_adjacency_matrices[0].shape[0] # assumes all graphs have the same number of nodes
+        partition_size = N // K
+        
+        partitions = [list(range(i, min(i + partition_size, N))) for i in range(0, N, partition_size)]
+
+
+        # for each partition pair, estimate edge probability, which is the average edge density between the two partitions
+        for i in range(K):
+            for j in range(K):
+                total_edge_density = 0
+                for adj_matrix in aligned_adjacency_matrices:
+                    partition_i = partitions[i]
+                    partition_j = partitions[j]
+                    step_function_matrix[i, j] += np.mean(adj_matrix[partition_i, :][:, partition_j])
+                step_function_matrix[i, j] = total_edge_density / len(aligned_adjacency_matrices)
+
+        return step_function_matrix
+
 
     def graphon_mixup():
         """
